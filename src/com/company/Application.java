e844682cba5a49a96a504b84b573b8d2263a1d5d
pernick.sayan <pernick.sayan@VA-1116-SC08.local>
2021-10-05 11:02:31 -0700

    Initial commit

---
+++ src/com/company/Application.java
@@ -0,0 +1,15 @@
+package com.company;
+
+public class Application {
+
+
+    public static void main(String[] args) {
+        System.out.println(squareOf(89));
+    }
+
+
+        // write your code here
+    public static int squareOf(int n) {
+        return n*n;
+    }
+}
